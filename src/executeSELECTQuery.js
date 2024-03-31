fb5dbe32dc3b772b51608a9769966b55df08bfd1
Abhishek Rajput <93138190+Abhi-0001@users.noreply.github.com>
2024-03-31 17:52:41 +0530

    feat: test upto 7 are completed

---
+++ src/executeSELECTQuery.js
@@ -0,0 +1,46 @@
+const readCSV = require("./csvReader");
+const parseQuery = require("./queryParser");
+
+async function executeSELECTQuery(query) {
+  const { fields, table, whereClauses } = parseQuery(query);
+  const data = await readCSV(`${table}.csv`);
+
+  // WHERE CLAUSE filtering of DATA
+  const filteredData =
+    whereClauses.length > 0
+      ? data.filter((row) => {
+          return whereClauses.every((clause) => evaluateCondition(row, clause));
+        })
+      : data;
+
+  // selecting the rows
+  return filteredData.map((row) => {
+    const selectedRow = {};
+    fields.forEach((field) => {
+      selectedRow[field] = row[field];
+    });
+    return selectedRow;
+  });
+}
+
+function evaluateCondition(row, clause) {
+  const { field, operator, value } = clause;
+  switch (operator) {
+    case "=":
+      return row[field] === value;
+    case "!=":
+      return row[field] !== value;
+    case ">":
+      return row[field] > value;
+    case "<":
+      return row[field] < value;
+    case ">=":
+      return row[field] >= value;
+    case "<=":
+      return row[field] <= value;
+    default:
+      throw new Error(`Invalid operator used ${operator}`);
+  }
+}
+
+module.exports = executeSELECTQuery;
--- src/queryParser.js
+++ src/queryParser.js
@@ -1,13 +1,31 @@
 module.exports = function parseQuery(query) {
-  const selectRegex = /SELECT (.+) FROM (.+)/i;
+  const selectRegex = /SELECT (.+?) FROM (.+?)(?: WHERE (.*))?$/i;
   const match = query.match(selectRegex);
   if (match) {
-    const [, fields, tableName] = match;
+    const [, fields, table, whereString] = match;
+    const whereClauses = whereString ? parseWhereClause(whereString) : [];
     return {
       fields: fields.split(",").map((field) => field.trim()),
-      tableName: tableName.trim(),
+      table: table.trim(),
+      whereClauses,
     };
   } else {
     return "Invalid query format";
   }
 };
+
+function parseWhereClause(whereString) {
+  const conditionRegex = /(.*?)(=|!=|>|<|>=|<=)(.*)/;
+
+  return whereString.split(/and|or/i).map((condStr) => {
+    const match = condStr.match(conditionRegex);
+    if (match) {
+      const [, field, operator, value] = match;
+      return {
+        field: field.trim(),
+        operator: operator.trim(),
+        value: value.trim(),
+      };
+    } else throw new Error("invalid WHERE clause format");
+  });
+}
